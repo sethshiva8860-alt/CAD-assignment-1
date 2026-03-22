451120b2951f93b812f9f6a9607d9b44aacb3af9
sethshiva8860-alt <sethshiva8860@gmail.com>
2026-03-23 00:53:14 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,9 @@
+# Function to divide two numbers
+def divide(a, b):
+    return a / b
+num1 = float(input("Enter first number: "))# Taking input from user
+num2 = float(input("Enter second number: "))
+
+result = divide(num1, num2)# Calling the function
+
+print("Division result is:", result)
