a7450ad37a481a6844e44b4130de9d5bffea9962
sethshiva8860-alt <sethshiva8860@gmail.com>
2026-03-23 00:50:57 +0530

    Add files via upload

---
+++ subtract.py
@@ -0,0 +1,9 @@
+# Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = subtract(num1, num2)# Calling the function
+
+print("Subtraction result is:", result)
