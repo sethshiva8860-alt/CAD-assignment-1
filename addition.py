301b819f0c53afa36bc57d1bb31e22cf0f7a17f7
sethshiva8860-alt <sethshiva8860@gmail.com>
2026-03-23 00:47:47 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,9 @@
+# Function to multiply two numbers
+def add(a, b):
+    return a + b
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+result = add(num1, num2)# Calling the function
+
+print("addition result is:", result)
